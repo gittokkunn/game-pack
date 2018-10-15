d0bf12c3171372aaeed22048f78490ee6b0f7570
tokkunn <s.tkh0215@gmail.com>
2018-10-15 19:48:50 +0900

    Updates

---
+++ precache-manifest.e4b8cc41173a43c27f7a8990022c1c36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef6034311d8fb2b16a31",
+    "url": "/game-pack/static/js/runtime~main.ef603431.js"
+  },
+  {
+    "revision": "4bf117b68b92678a252b",
+    "url": "/game-pack/static/js/main.4bf117b6.chunk.js"
+  },
+  {
+    "revision": "d74436f1ea2a2d333bd2",
+    "url": "/game-pack/static/js/1.d74436f1.chunk.js"
+  },
+  {
+    "revision": "4bf117b68b92678a252b",
+    "url": "/game-pack/static/css/main.6faaec90.chunk.css"
+  },
+  {
+    "revision": "dceb8edb89df36bb7f42b7b27dd7e302",
+    "url": "/game-pack/index.html"
+  }
+];
